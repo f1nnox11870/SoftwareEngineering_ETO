f35a778f766f774355c7ee7c38ccf526eb408330
Naphat <bori.jung125@gmail.com>
2026-04-01 07:43:15 +0700

    fixing missing letter

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../assets/history.css';
+import '../assets/History.css';
 import Navbar from './navbar';
 
 // ── Constants ──
